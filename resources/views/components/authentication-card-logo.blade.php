71edd6d74c6ecef3f8a79508bed07e544e530813
Aldo Hartanto <aldohartanto06@gmail.com>
2024-06-20 20:43:59 +0700

    User admin

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{ asset('assets/images/logo.png') }}" class="w-30 h-16" />
+</a>
---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ config('app.url') }}">
+    <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }} Logo" class="w-35 h-20">
+</a>
